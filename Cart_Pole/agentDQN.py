9a56d68c794a83dd82617fe5f4048fdea06352b2
Hoang-Son NGUYEN <son@MacBook-Pro-de-Hoang-Son.local>
2023-05-27 19:59:46 +0200

    add fit DQN

--- Cart_Pole/agentDQN.py
+++ Cart_Pole/agentDQN.py
@@ -21,22 +21,30 @@ class Buffer:
         self.memoire.append(element)
 
 class DQN(torch.nn.Module):
-    def __init__(self, taille_state, taille_action):
+    def __init__(self, taille_state, taille_action, lr = 1e-3, epoch=25):
         super().__init__()
         self.taille_state = taille_state
         self.taille_action = taille_action
+        self.lr = lr
+        self.epoch = epoch
         self.net = torch.nn.Sequential(
                         torch.nn.Linear(self.taille_state, TAILLE_LATENT),
                         torch.nn.ReLU(),
                         torch.nn.Linear(TAILLE_LATENT, self.taille_action)
         )
-        self.optim = torch.optim.Adam(self.parameters(), lr = 0.001)
-        print(self.parameters())
+        self.optim = torch.optim.Adam(self.parameters(), lr = self.lr)
+        self.f_loss = torch.nn.MSELoss()
 
     def forward(self, x):
         return self.net(x)
     
-
+    def fit(self, x, y):
+        for _ in range(self.epoch):
+            y_hat = self.forward(x)
+            loss = self.f_loss(y_hat, y)
+            loss.backward()
+            self.optim.step()
+            self.optim.zero_grad()
 
 class AgentDQN(torch.nn.Module):
     def __init__(self, taille_state, taille_action):
@@ -51,5 +59,3 @@ class AgentDQN(torch.nn.Module):
         
     def act(self, state):
         return torch.argmax(self.net(state))
-    
-dqn = DQN(3,4)
